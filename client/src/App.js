39832a91372899bfd0fea27d50f84cc81d671192
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-05-04 22:13:21 +1000

    APP WIP and some file structure added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
-import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-     first commit
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<Home />} 
+          />
+          <Route 
+            path='/saved' 
+            element={< />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+    <Footer />
+ 
+  </ApolloProvider>
   );
 }
 
